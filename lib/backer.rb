78a1c71d04ef3350fa80817d44ef98a70267279b
github username <github email address>
2019-08-08 14:14:05 -0400

    solution with instance variables

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,10 +1,14 @@
 class Backer
-
-    attr_reader :name, backed_projects
-
+    attr_reader :name, :backed_projects
+  
     def initialize(name)
-        @name = name
-        @backed_projects = []
+      @name = name
+      @backed_projects = []
     end
-
-end
+  
+    def back_project(project)
+      @backed_projects << project
+      project.backers << self
+    end
+  
+  end
--- lib/project.rb
+++ lib/project.rb
@@ -1,10 +1,14 @@
 class Project
-
-    attr_reader :title, backers
-
+    attr_reader :title, :backers
+  
     def initialize(title)
-        @title = title
-        @backers = []
+      @title = title
+      @backers = []
     end
-
-end
+  
+    def add_backer(backer)
+      backers << backer
+      backer.backed_projects << self
+    end
+  
+  end
